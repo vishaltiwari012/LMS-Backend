f3e34c2876442c7bbcf670614ff58153077ae39b
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-06-17 21:17:34 +0530

    add dotenv and cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 dotenv.config({path : "./config/config.env"});
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cors());
+
 export default app;
