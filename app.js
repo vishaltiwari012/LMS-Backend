986a886623448a26e81c2c4affaa77fdd4acf3f2
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-06-18 21:00:19 +0530

    add starter files

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
 import { connectDB } from './database/dbConnection.js';
+import { errorMiddleWare } from './middlewares/error.js';
 
 const app = express();
 dotenv.config({path : "./config/config.env"});
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use(cors());
+app.use(cookieParser());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : "/tmp/",
+}))
+
+
+
 
 connectDB();
+app.use(errorMiddleWare);
 export default app;
